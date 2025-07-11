9084e712a2aa283de3edb8eced4559cfa4344adc
sabyun <hoone0802@gmail.com>
2025-07-11 17:11:34 +0900

    02

--- cody/02/power_calculator.py
+++ cody/02/power_calculator.py
@@ -6,13 +6,17 @@ def pow_calculaor (pow :int , value :float):
 
 
 def main ():
-    value = float(input("제곱할 값을 입력하세요"))
-    pow = int(input("제곱 횟수를 입력하세요"))
+    value = float(input("Enter number: "))
+    if not isinstance(value, float):
+        return "float 값이 아닙니다"
+    pow = int(input("Enter exponent: "))
+    if not isinstance(pow, int):
+        return "int 값이 아닙니다"
     if(pow < 0 or value < 0):
         return "입력값오류"
     result = pow_calculaor(pow, value)
     
-    print(f"값은 {result}입니다.")
+    print(f"Result: {result}")
     
 if __name__ == "__main__":
     main()
