1f43c078fee3a823e28ccb5c9aa1b397c2382a5d
sabyun <hoone0802@gmail.com>
2025-07-14 20:01:14 +0900

    first commit

--- cody/05/sort_calculator.py
+++ cody/05/sort_calculator.py
@@ -1,6 +1,7 @@
 def quick_sort(arr):
     if len(arr) <= 1:
         return arr
+    
     pivot = arr[0]
     left = [x for x in arr[1:] if x < pivot]
     right = [x for x in arr[1:] if x >= pivot]
